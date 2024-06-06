b5b7c1fed05e16c131abd6733a8fe65a0b645582
AlvinRHD <ezequielhernandes907@gmail.com>
2024-06-06 11:22:00 -0600

    Estructure project

---
+++ Solitario_Piramide/UI/Menu.cs
@@ -0,0 +1,24 @@
+﻿using Spectre.Console;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solitario_Piramide.UI
+{
+    public static class Menu
+    {
+        public static void ShowWelcomeMenu()
+        {
+            AnsiConsole.Write(new FigletText("Pyramid Solitaire")
+                .Centered()
+                .Color(Color.Green));
+
+            AnsiConsole.Write(new Markup("[bold yellow]Welcome to Pyramid Solitaire![/]"));
+            AnsiConsole.WriteLine();
+            AnsiConsole.Write(new Markup("Press [bold green]Enter[/] to start the game."));
+            Console.ReadLine();
+        }
+    }
+}
